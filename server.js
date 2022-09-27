e589bb5c397ae177b1790dcb9c6384264b1adbe1
mauriGc <mauricioogarcia1067@gmail.com>
2022-09-27 14:20:24 -0300

    probando

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const { Server: HTTPServer } = require("http");
+const { Server: IOServer } = require("socket.io");
+const { addAbortSignal } = require("stream");
+
+
+const app = express();
+const httpServer = new HTTPServer(app);
+const io = new IOServer(httpServer);
+
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => {
+    res.sendFile("index.html");
+})
+
+
+io.on("connection", (socket) => {
+    console.log("Nuevo cliente conectado.")
+})
+
+
+// Puerto 
+const PORT = 8080;
+httpServer.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}`)
+})
